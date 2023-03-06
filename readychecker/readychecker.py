8d9691c3c3e8fcd1de83d552b6a0f12745342181
Artemis6969 <82315185+Artemis6969@users.noreply.github.com>
2023-03-06 17:41:28 +0100

    test 1

--- readychecker/readychecker.py
+++ readychecker/readychecker.py
@@ -12,16 +12,16 @@ class Confirm(discord.ui.View):
     # When the confirm button is pressed, set the inner value to `True` and
     # stop the View from listening to more input.
     # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
+    @discord.ui.button(label='Ready', style=discord.ButtonStyle.green)
     async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Confirming', ephemeral=True)
+        await interaction.response.send_message('Adding you to the ready list', ephemeral=True)
         self.value = True
         self.stop()
 
     # This one is similar to the confirmation button except sets the inner value to `False`
-    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
+    @discord.ui.button(label='Ignore', style=discord.ButtonStyle.grey)
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Cancelling', ephemeral=True)
+        await interaction.response.send_message('Ignoring readycheck', ephemeral=True)
         self.value = False
         self.stop()
 
@@ -33,17 +33,18 @@ class ReadyChecker(commands.Cog):
     @commands.command(description="Check if your homies are ready")
     async def readycheck(self, ctx: commands.Context):
         """Check if your homies are ready"""
-        #for mem in ctx.guild.members():
-        view = Confirm()
-        await ctx.send('Do you want to continue?', view=view)
-        # Wait for the View to stop listening for input...
-        await view.wait()
-        if view.value is None:
-            await ctx.send("Timedout")
-        elif view.value:
-            await ctx.send('Confirmed...')
-        else:
-            await ctx.send('Cancelled...')
+        readyhomies = []
+        for mem in ctx.guild.members():
+            view = Confirm()
+            await mem.send("Will you be ready tonight?, You have 30s to reply", view=view)
+            await view.wait()
+            if view.value:
+                readyhomies.append(mem.name)
+            else:
+                pass
+            await asyncio.sleep(2)
+
+        await ctx.send(f"Ready homies: {readyhomies}")
 
 
             
