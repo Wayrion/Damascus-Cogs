d713a524a384ce790c2b5a4308a854b3f1dd7597
Artemis6969 <82315185+Artemis6969@users.noreply.github.com>
2023-03-06 15:41:44 +0100

    i havent coded in a while dont judge

---
+++ readychecker/__init__.py
@@ -0,0 +1,4 @@
+from .readychecker import ReadyChecker
+
+async def setup(bot):
+    await bot.add_cog(ReadyChecker(bot))
---
+++ readychecker/readychecker.py
@@ -0,0 +1,51 @@
+import discord
+from discord import Interaction, app_commands
+from redbot.core import commands
+from redbot.core import Config
+import asyncio
+
+class Confirm(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Confirming', ephemeral=True)
+        self.value = True
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Cancelling', ephemeral=True)
+        self.value = False
+        self.stop()
+
+class ReadyChecker(commands.Cog):
+    """Checks which homies are ready tonight"""
+    def __init__(self, bot):
+        self.bot = bot
+            
+    @app_commands.command(description="Check if your homies are ready")
+    async def readycheck(self, ctx: commands.Context):
+        """Check if your homies are ready"""
+        #for mem in ctx.guild.members():
+        view = Confirm()
+        await ctx.send('Do you want to continue?', view=view)
+        # Wait for the View to stop listening for input...
+        await view.wait()
+        if view.value is None:
+            await ctx.send("Timedout")
+        elif view.value:
+            await ctx.send('Confirmed...')
+        else:
+            await ctx.send('Cancelled...')
+
+
+            
+            
+   
