5b7111c71efb3afa2cde912c657f476d702bb92b
Artemis6969 <82315185+Artemis6969@users.noreply.github.com>
2024-07-05 17:07:56 +0200

    feat: update nodms to Red v3.5

---
+++ nodms/__init__.py
@@ -0,0 +1,10 @@
+import json
+from pathlib import Path
+from .nodms import NoDMs
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+async def setup(bot):
+    await bot.add_cog(NoDMs(bot))
