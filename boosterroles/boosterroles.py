c25aa41ad9ab0e4a5bd512eb87def7424e54238c
Artemis6969 <82315185+Artemis6969@users.noreply.github.com>
2024-08-03 19:30:44 +0200

    fix: add role when assign command is run for the first time.

--- boosterroles/boosterroles.py
+++ boosterroles/boosterroles.py
@@ -488,6 +488,7 @@ class BoosterRoles(commands.Cog):
                 if role_position:
                     await asyncio.sleep(5)
                     await role.edit(position=role_position)
+                await ctx.author.add_roles(role)
                 await self.config.member(ctx.author).role_data.set(role.id)
 
             else:
